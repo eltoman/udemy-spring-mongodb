a75799b3a41ddd5538b5868490b7c3b474e965d0
eltoman <eltomds@usp.br>
2020-06-08 20:14:46 -0300

    Add post class

--- src/main/java/com/workshopmongo/config/Instantiation.java
+++ src/main/java/com/workshopmongo/config/Instantiation.java
@@ -1,6 +1,9 @@
 package com.workshopmongo.config;
 
+import com.workshopmongo.domain.Post;
 import com.workshopmongo.domain.User;
+import com.workshopmongo.dto.AuthorDTO;
+import com.workshopmongo.repository.PostRepository;
 import com.workshopmongo.repository.UserRepository;
 import com.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,9 @@ public class Instantiation implements CommandLineRunner {
 private UserRepository userReposiroty;
 
 @Autowired
+private PostRepository postReposiroty;
+
+@Autowired
 private UserService userService;
 
     @Override
@@ -28,15 +34,26 @@ private UserService userService;
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
+        //userReposiroty.deleteAll();
+        //postReposiroty.deleteAll();
+
         List<User> users = new ArrayList<>();
         users.add(new User(null, "Maria Brown", "maria@gmail.com"));
         users.add(new User(null, "Yanna Maria", "yanna@gmail.com"));
         users.add(new User(null, "Obama", "obaminha@gmail.com"));
 
         for (User user : users){
-            if (!userService.isEmailRegistered(user.getEmail()))
-                userReposiroty.save(user);
+            if (!userService.isEmailRegistered(user.getEmail())) {
+                user = userReposiroty.save(user);
+                AuthorDTO author = new AuthorDTO(user);
+                Post post1 = new Post(null, sdf.parse("21/03/2018"),  "Partiu viagem " + user.getName() , "Vou viajar para São Paulo. Abraços!", author);
+                Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia " + user.getName(), "Acordei feliz hoje!", author);
+
+                postReposiroty.saveAll(Arrays.asList(post1, post2));
+            }
         }
+
+
     }
 
 }
---
+++ src/main/java/com/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.workshopmongo.repository;
+
+import com.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
